992f6b7f8a2ef67b6fbdef4ae25b8eea62ea0d76
Santy_M <santy15021502@gmail.com>
2025-06-26 15:06:25 -0300

    90% login

---
+++ js/login.js
@@ -0,0 +1,35 @@
+const userButton = document.querySelector('.user');
+const userAdmin = document.querySelector('.admin');
+const formulario = document.querySelector('.formulario');
+
+const cambiarClases = (e) =>{
+    if (e) {
+        e.preventDefault();
+    }
+    let who = '';
+    if(userButton.classList.contains('select')) {
+        userAdmin.classList.add('select');
+        userButton.classList.remove('select');
+        who = 'Numero de Administrador';
+    }else if(userAdmin.classList.contains('select')) {
+        userButton.classList.add('select');
+        userAdmin.classList.remove('select');
+        who = 'Nombre de Usuario';
+    }
+    formulario.innerHTML = `
+    <div class="inputs-box">
+        <div class="inputs-inputs">
+            <label for="nombre">${who}</label>
+            <input type="text" id="nombre">
+            <label for="contrasena">Contrasena</label>
+            <input type="password" id="contrasena">
+        </div>
+        <div class="button-box">
+            <button class="button-login">Log In</button>
+        </div>
+    </div>`;
+}
+
+cambiarClases();
+userButton.addEventListener('click', cambiarClases);
+userAdmin.addEventListener('click', cambiarClases);
